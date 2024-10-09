13cb53ce7c295dad655f9b3e44ec4ee0c1fb0c67
Joern Duefelsiek <joern.duefelsiek@weidmueller.com>
2024-10-09 13:22:18 +0200

    fix copy paste error

--- macros/database_specific_helpers/split_part.sql
+++ macros/database_specific_helpers/split_part.sql
@@ -1,5 +1,5 @@
 {% macro column_identifier(column_index) -%}
-  {{ return(adapter.dispatch('column_identifier')(column_index)) }}
+  {{ return(adapter.dispatch('split_part')(column_index)) }}
 {%- endmacro %}
 
 {% macro fabric__split_part(string_text, delimiter_text, part_number) -%}
